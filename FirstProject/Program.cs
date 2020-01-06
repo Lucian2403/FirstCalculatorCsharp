4086dbf45f5608b73b97d591f66ce1241fe51a0c
Lucian <lucianciubotaru2403@gmail.com>
2020-01-06 13:15:10 +0200

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose which operation you would like to make: +, - , * , /");
+
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("You choose plus");
+                    break;
+
+                case "-":
+                    Console.WriteLine("You choose minus");
+                    break;
+
+                case "*":
+                    Console.WriteLine("You choose multiply");
+                    break;
+
+                case "/":
+                    Console.WriteLine("You choose divide");
+                    break;
+                default:
+                    Console.WriteLine("Choose a proper operation, please!");
+                    break;
+            }
+
+
+        }
+      
+    }
+}
